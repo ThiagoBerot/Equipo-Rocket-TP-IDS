4b15533acd889b583c66ee74a57e786963fb7f18
renzomartin1 <rsmartin@fi.uba.ar>
2024-06-04 17:18:39 -0300

    Backend
    Agrego un directorio para backend. Tiene el codigo del front y el mapa incluido

---
+++ Backend/static/js/script.js
@@ -0,0 +1,16 @@
+
+function mostrarContenido(id) {
+    // Ocultar todas las secciones
+    var secciones = document.querySelectorAll('.content section');
+    secciones.forEach(function (seccion) {
+        seccion.style.display = 'none';
+    });
+
+    // Mostrar solo la sección seleccionada
+    var seccionMostrar = document.getElementById(id);
+    seccionMostrar.style.display = 'block';
+
+
+}
+
+
