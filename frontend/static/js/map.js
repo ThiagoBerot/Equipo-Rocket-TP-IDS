f812e6913543fcafbb0e6dfd752dd05019383bd4
renzomartin1 <rsmartin@fi.uba.ar>
2024-06-11 19:27:11 -0300

    Agregro codigo en map.js

--- frontend/static/js/map.js
+++ frontend/static/js/map.js
@@ -4,8 +4,14 @@
  * 3 y la que filtra los datos y hae zoom a un marccador en especifico.
  */
 
-const base_de_datos = [{ lat: -34.5956145, lng: -58.4431949 }];
+const base_datos_coordenadas = [{ lat: -34.5956145, lng: -58.4431949 }, { lat: -34.60342, lng: -58.38525 }];
 
+const base_datos_formulario = 
+  [{'id':1,'animal':'perro','edad':'adulto','raza':'labrador','color':'negro',
+    'tamanio':'grande','sexo':'macho','mail':'rsmartin@fi.uba.ar','descripcion':'inquieto',
+    'fecha_encuentro':'13/07/2024'}, {'id':2,'animal':'gato','edad':'cachorro','raza':'siames','color':'blanco',
+    'tamanio':'chico','sexo':'hembra','mail':'rsmartin@fi.uba.ar','descripcion':'inquieto',
+    'fecha_encuentro':'12/07/2024'}];
 /*-----------------------------------------------------------------------------------*/
 function iniciarMap() {
   let coord = { lat: -34.5956145, lng: -58.4431949 };
@@ -15,6 +21,7 @@ function iniciarMap() {
   });
   //Cada vez que se inicia el mapa tiene que consultar una base de datos para que
   //le traiga todas las coordenadas y levantarlas en el mapa
+  //function recibirDatos(latitud,longitud);
   dibujarTodosLosMarcadores(map);
 }
 /*---------------------------------------------------------------------------------------*/
@@ -22,28 +29,43 @@ const boton_encontraste_mastcota = document.getElementById("boton-formulario")
 boton_encontraste_mastcota.addEventListener("click", function(event){
   let latitud = $("#lat").val()
   let longitud = $("#long").val()
-  // funtion enviarDatos(latitud,longitud);
+  // function enviarDatos(latitud,longitud);
 })
+
+/*---------------------------------------------------------------------------------------*/
+function cargarIcono(especie){
+  let imagen;
+  if(especie == 'perro'){
+    imagen = {
+    url: '/static/img_map/perro.png',
+    size: new google.maps.Size(20, 32)
+    }
+  }
+  else{
+    imagen = {
+    url: '/static/img_map/gato.png',
+    size: new google.maps.Size(20, 32)
+    } 
+  }
+  return imagen;
+}
 /*---------------------------------------------------------------------------------------*/
 //Dibuja una marcador
-function dibujarMarcador(map, coord){
+function dibujarMarcador(map,coord,especie){
   let marker = new google.maps.Marker({
       position: coord,
-      map: map
+      map: map,
+      icon: cargarIcono(especie)
     });
   }
 /*---------------------------------------------------------------------------------------*/
 function dibujarTodosLosMarcadores(map){
-  console.log(base_de_datos.length);
-  for(let i=0; i < base_de_datos.length; i++){
-
-  let coord = base_de_datos[i];
-
-    let marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-    dibujarMarcador(map,coord);
+  let longitud_base_coordenadas = base_datos_coordenadas.length;
+  for(let i=0; i < longitud_base_coordenadas; i++){
+  let coord = base_datos_coordenadas[i];
+  let especie = base_datos_formulario[i]['animal'];
+  
+    dibujarMarcador(map,coord,especie);
   }
 }
 /*-------------------------------------------------------------------------------------- */
