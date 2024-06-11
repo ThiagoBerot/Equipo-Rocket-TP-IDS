bb1f672aff0d4ef9eaf42cacbf5bab1e99c09443
renzomartin1 <rsmartin@fi.uba.ar>
2024-06-11 03:09:48 -0300

    Update map.js

--- frontend/static/js/map.js
+++ frontend/static/js/map.js
@@ -1,6 +1,11 @@
-//let map;
-//let coord;
-let markers = [];
+
+/* 1 Falta hacer funcion que envia datos a bd cuando envia el formulario, 
+ * 2 la que recibe los datos cuando inicia el mapa para levantarlos en el mapa,
+ * 3 y la que filtra los datos y hae zoom a un marccador en especifico.
+ */
+
+const base_de_datos = [{ lat: -34.5956145, lng: -58.4431949 }];
+
 /*-----------------------------------------------------------------------------------*/
 function iniciarMap() {
   let coord = { lat: -34.5956145, lng: -58.4431949 };
@@ -8,46 +13,39 @@ function iniciarMap() {
     zoom: 10,
     center: coord
   });
-
   //Cada vez que se inicia el mapa tiene que consultar una base de datos para que
   //le traiga todas las coordenadas y levantarlas en el mapa
-  //dibujarTodosLosMarcadores(map,coord,markers)
-
-  //dibujarMarcador(map, coord, markers);
-  //addPanToMarker(map, markers);
+  dibujarTodosLosMarcadores(map);
 }
-/*-----------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------*/
 const boton_encontraste_mastcota = document.getElementById("boton-formulario")
 boton_encontraste_mastcota.addEventListener("click", function(event){
   let latitud = $("#lat").val()
   let longitud = $("#long").val()
-  let coord = { lat: latitud, lng: longitud }
-  let map = new google.maps.Map(document.getElementById("Mapa"), {
-    zoom: 10,
-    center: coord
-  });
-
-  dibujarMarcador(map, coord, markers)
-  
+  // funtion enviarDatos(latitud,longitud);
 })
-/*-----------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------*/
 //Dibuja una marcador
-function dibujarMarcador(map, coord, markers){
-let marker = new google.maps.Marker({
-    position: coord,
-    map: map
-  });
-  markers.push(marker);
-}
-/*--------------------------------------------------------------------------------------- */
-//Agrega evento a marcador
-/*function addPanToMarker(map, markers) {
-    markers.map(marker => {
-      marker.addListener('click', event => {
-          alert("Hola mundo");
-      });
+function dibujarMarcador(map, coord){
+  let marker = new google.maps.Marker({
+      position: coord,
+      map: map
     });
-}*/
+  }
+/*---------------------------------------------------------------------------------------*/
+function dibujarTodosLosMarcadores(map){
+  console.log(base_de_datos.length);
+  for(let i=0; i < base_de_datos.length; i++){
+
+  let coord = base_de_datos[i];
+
+    let marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+    dibujarMarcador(map,coord);
+  }
+}
 /*-------------------------------------------------------------------------------------- */
 /*Funcion que BUSCA el marker correcto segun el filtro,
 le va a preguntar a la base los datos seleccionados para
@@ -76,14 +74,9 @@ que le pida a otra base las coordenadas de la descripcion.*/
   let fecha = filtroFecha.value;
   document.getElementById('idFecha').innerText = `Ud. ha seleccionado la raza ${fecha}`;
 }*/
-/*--------------------------------------------------------------------------------- */
-/*Funcion que AGREGA el marker correcto segun el registro,
-va a agregar a la base los datos ingresados para
-poder mostrarlos en el mapa en las coordenadas correctas.*/
 
-//Agrega un marcador
-
-/*-------------------------------------------------------------------------------*/
+  
+/*-----------------------------------------------------------------------------------*/
 //Dibuja un circulo
 /*function drawCircle(map, coord) {
   const circleOptions = {
@@ -96,4 +89,14 @@ poder mostrarlos en el mapa en las coordenadas correctas.*/
   }
   const circle = new google.maps.Circle(circleOptions);
   return circle;
+}*/
+
+/*----------------------------------------------------------------------------------*/
+//Agrega evento a marcador
+/*function addPanToMarker(map, markers) {
+    markers.map(marker => {
+      marker.addListener('click', event => {
+          alert("Hola mundo");
+      });
+    });
 }*/
