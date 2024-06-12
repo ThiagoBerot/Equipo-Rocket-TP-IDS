3267c88aeb5e1e48e8dece54062a4be111db8b6c
ThiagoBerot <tberot@fi.uba.ar>
2024-06-11 21:12:30 -0300

    Tarea #3: adiciones al formulario y nueva vista de formulario enviado
    Se agregaron dos inputs para que el usuario ingrese la zona donde encontró la mascota perdida (latitud y longitud) y otro aparte para que indique el nombre de la mascota encontrada. Se agregó una nueva vista de formulario enviado al hacer submit, se agregaron funciones a app.py para renderizar estas mismas. También, se cambió la forma de linkear a imagenes a otras vistas usando url_for() en la vista encontraste_una_mascota.html

--- front+back/frontend/app.py
+++ front+back/frontend/app.py
@@ -15,8 +15,27 @@ def mascotas():
     return render_template('mascotas.html')
 
 @app.route('/encontraste')
-def encontraste():
+def encontraste():    
     return render_template('encontraste_una_mascota.html')
+
+@app.route('/enviado', methods=["POST"])
+def enviado():
+        if request.method == "POST":
+            nombre = request.form.get('nombre_animalf')
+            animal = request.form.get('animalf')
+            edad = request.form.get('edadf')
+            raza = request.form.get('raza_animalf')
+            color = request.form.get('colorf')
+            tamaño = request.form.get('tamañof')
+            sexo = request.form.get('sexof')
+            zona_latitud = request.form.get('zona_latitudf')
+            zona_longitud = request.form.get('zona_longitudf')
+            descripcion = request.form.get('descripcionf')
+            fecha = request.form.get('fecha_mascotaf')
+            mail = request.form.get('user_mailf')
+            datos = [nombre, animal, edad, raza, color, tamaño, sexo, zona_latitud, zona_longitud, descripcion, fecha, mail]
+
+            return render_template('formulario_enviado.html', datos=datos)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
