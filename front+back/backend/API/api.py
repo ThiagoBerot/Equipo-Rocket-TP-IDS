6d9e15ddd0427575ea3067c99f23ce789bc5bf0c
usarnamerodrigoisnotavailable <ralopez@fi.uba.ar>
2024-06-13 09:40:57 -0300

    se modifico la connecion con la base de datos y se agrego la consulta a la base de datos

--- front+back/backend/API/api.py
+++ front+back/backend/API/api.py
@@ -73,31 +73,57 @@ def set_connection():
     #funcion que conecta a la base de datos
     engine = create_engine("mysql+mysqlconnector://user1:lizi12@localhost/mascota")
     connection = engine.connect()
+
     return connection
 
 @app.route('/form', methods=['POST'])
 def registrarForm():
     conn = set_connection()
     result = conn
-    #nombre                = request.form['nombre']
+    nombre                = request.form['nombre']
     tipo                  = request.form['tipo']
     edad                  = request.form['edad']
     raza                  = request.form['raza']
     color                 = request.form['color']
     tamanio               = request.form['tamanio']
     sexo                  = request.form['sexo']
-    zona                  = request.form['zona']
+    latitud               = request.form['latitud']
+    longitud              = request.form['longitud']
     mail                  = request.form['mail']
     descripcion           = request.form['descripcion']
-    mail                  = request.form['mail']
-    #fecha_desaparicion    = request.form['fecha_desaparicion']
-    fecha_encontrado    = request.form['fecha_encontrado']
-    query= f"""INSERT INTO perdido(nombre, tipo, edad, raza, color, tamanio, sexo, zona, mail, descripcion) VALUES ('lizi','{request.form["tipo"]}','{request.form["edad"]}', '{request.form["raza"]}', '{request.form["color"]}','{request.form["tamanio"]}','{request.form["sexo"]}','{request.form["zona"]}', '{request.form["fecha_encontrado"]}', '{request.form["fecha_encontrado"]}')"""  
+    fecha_encontrado    = request.form['fecha_encontrado']#fecha en la que la mascota se encontro en la via publica
+    query= f"""INSERT INTO perdido(nombre, tipo, edad, raza, color, tamanio, sexo, latitud, longitud, mail, descripcion) VALUES ('{request.form["nombre"]}','{request.form["tipo"]}','{request.form["edad"]}', '{request.form["raza"]}', '{request.form["color"]}','{request.form["tamanio"]}','{request.form["sexo"]}','{request.form["latitud"]}', '{request.form["longitud"]}', '{request.form["fecha_encontrado"]}')"""  
     result.execute(text(query))
     conn.commit() #"confirmar" cambios a la base
     conn.close() #cerrando conexion SQL
     result.close() #cerrando conexion de la base 
     return render_template('formulario_enviado.html'),200
+@app.route('/mascotas',methods=["GET"])
+def mostrar_mascotas():
+    conn=set_connection()
+    data=[]
+    query="SELECT * FROM perdido;"
+    try:
+        result= conn.execute(text(query))
+    except SQLAlchemyError as err:
+        print("error",err.__cause__)
+    for row in result:
+        dicc = {}
+        dicc['nombre'] = row.nombre
+        dicc['tipo'] = row.nombre
+        dicc['edad'] = row.nombre
+        dicc['raza'] = row.nombre
+        dicc['color'] = row.nombre
+        dicc['tamanio'] = row.nombre
+        dicc['sexo'] = row.nombre
+        dicc['latitud'] = row.nombre
+        dicc['longitud'] = row.nombre
+        dicc['mail'] = row.nombre
+        dicc['descripcion'] = row.nombre
+        dicc['fecha_encontrado'] = row.nombre
+        data.append(dicc)
+    return jsonify(data),200
+    
 
 '''
 
