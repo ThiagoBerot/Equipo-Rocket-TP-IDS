31525e51389c5f77e9838a3614cd4d390458d2e9
Rocío Lorenzo <rlorenzo@fi.uba.ar>
2024-06-13 18:34:17 -0300

    .venv en init

--- front+back/init.sh
+++ front+back/init.sh
@@ -1,11 +1,16 @@
-#Dependencias
+#Creación entorno virtual con flask
+cd frontend
+mkdir .venv
+pipenv install flask
+pipenv shell
 
 #Dependencias necesarias para conectar a mysql desde flask con xampp.
 pip3 install flask_sqlalchemy
 pip3 install mysql-connector-python
 
-#Levantar contenedor y levantarse a la base de datos 
-cd docker
+#Levantar contenedor y levantarse a la base de datos
+cd ..
+cd backend/API/docker
 sudo docker-compose up --build -d
 sudo docker exec -it docker-db-1 mysql -u root -p
 use mascotas;
